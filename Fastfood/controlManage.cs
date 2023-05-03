7033e24abf54a803176a7c54aee8c0c428c86dc4
blueorion05 <jhudielatienza2k20@gmail.com>
2023-05-03 14:18:22 +0800

    reset

--- Fastfood/controlManage.cs
+++ Fastfood/controlManage.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,33 +13,19 @@ namespace Fastfood
 {
     public partial class controlManage : UserControl
     {
-        String db = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\blueorion05\Fastfood\Fastfood\Database\Login.accdb";
-        String addData = "INSERT INTO FoodAndDrinks (Category, Item, Price) VALUES (?,?,?)";
-        public OleDbConnection conn = new OleDbConnection();
-        string FoodAndDrinks = "SELECT * FROM FoodAndDrinks";
         public controlManage()
         {
-            conn = new OleDbConnection(db);
-
             InitializeComponent();
         }
 
         private void controlManage_Load(object sender, EventArgs e)
         {
-            TableData();
+
         }
 
         private void TableData()
         {
-            conn.Open();
-            OleDbCommand cmd = new OleDbCommand(FoodAndDrinks, conn);
-            OleDbDataReader reader = cmd.ExecuteReader();
-            tableItems.Rows.Clear();
-            while (reader.Read())
-            {
-                tableItems.Rows.Add(reader[0].ToString(), reader[1].ToString(), reader[2].ToString(), reader[3].ToString());
-            }
-            conn.Close();
+
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -61,33 +46,11 @@ namespace Fastfood
         private void btnAdd_Click(object sender, EventArgs e)
         {
 
-            OleDbCommand addDataCommand;
-            addDataCommand = new OleDbCommand(addData, conn);
-            addDataCommand.Parameters.AddWithValue("Category", OleDbType.VarChar).Value = tbCategory.Text;
-            addDataCommand.Parameters.AddWithValue("Item", OleDbType.VarChar).Value = tbItem.Text;
-            addDataCommand.Parameters.AddWithValue("Price", OleDbType.VarChar).Value = tbPrice.Text;
-         
-            conn.Open();
-            int dataInserted = addDataCommand.ExecuteNonQuery();
-
-            if (dataInserted > 0)
-            {
-                MessageBox.Show("Added");
-                conn.Close();
-                TableData();
-            }
-            conn.Close();
         }
 
         private void btnBrowse_Click(object sender, EventArgs e)
         {
-            OpenFileDialog imgDialog = new OpenFileDialog();
-            DialogResult imgResult = imgDialog.ShowDialog();
-            if (imgResult == DialogResult.OK)
-            {
-                string fileName = imgDialog.FileName;
-                pictureBox1.Image = Image.FromFile(fileName);
-            }
+
         }
     }
 }
--- Fastfood/formLogin.cs
+++ Fastfood/formLogin.cs
@@ -1,53 +1,18 @@
-using System.Data;
-using System.Data.OleDb;
-using System.Security.Cryptography;
-
 namespace Fastfood
 {
     public partial class formLogin : Form
     {
-        private OleDbConnection conn = new OleDbConnection();
         public formLogin()
         {
             InitializeComponent();
-
-            conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\blueorion05\Fastfood\Fastfood\Database\Login.accdb;
-            Persist Security Info=False";
         }
-
-        
 
         private void btnlogin_Click(object sender, EventArgs e)
         {
-            conn.Open();
-            OleDbCommand cmd = new OleDbCommand();
-            cmd.Connection = conn;
-            cmd.CommandText = "select * from Account where username = '" + tbuser.Text + "' and password='" + tbpass.Text + "'";
-
-            OleDbDataReader or = cmd.ExecuteReader();
-
-            int count = 0;
-            while (or.Read())
-            {
-                count += 1;
-            }
-            if (count == 1)
-            {
-                conn.Close();
-                this.Hide();
-                formMain f = new formMain();
-                f.ShowDialog();
-                this.Show();
-            }
-            else
-            {
-                conn.Close();
-                MessageBox.Show("Enter valid username/password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                tbuser.Text = null;
-                tbpass.Text = null;
-            }
-
-
+            this.Hide();
+            formMain f = new formMain();
+            f.ShowDialog();
+            this.Show();
         }
 
         private void btnexit_Click_1(object sender, EventArgs e)
