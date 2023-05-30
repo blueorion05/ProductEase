f6302569c9817f3b5f2dc9d569f5df65aa33178e
Benjie Cabael <Benjie Cabael@LAPTOP-TCI8S1KJ>
2023-05-30 23:09:42 +0800

    code clean up

--- Fastfood/Controls/Home.cs
+++ Fastfood/Controls/Home.cs
@@ -17,21 +17,6 @@ namespace Fastfood
             InitializeComponent();
         }
 
-        private void lbl2totalorders_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lbl3totalsales_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void controlHome_Load(object sender, EventArgs e)
         {
             formMain f = new formMain();
@@ -44,22 +29,7 @@ namespace Fastfood
 
         }
 
-        private void pb1totalorders_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void lbl2totalordersnumbers_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
         {
 
         }
